a352545092c94ba49921d264f6be21e2f3a84578
Developer23-q <dailydrivex89@gmail.com>
2026-06-22 02:32:55 +0530

    chore: add cloudflare redirects rule

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,19 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  // 1. DYNAMIC FIX: Enable Nitro compiling ONLY when on Vercel
+  nitro: process.env.VERCEL ? true : false,
+
   tanstackStart: {
     server: { entry: "server" },
   },
   vite: {
-    // 1. Tell Vite to accept the ngrok proxy connection
     server: {
-      allowedHosts: ["pebbly-mutilator-luxury.ngrok-free.dev"] 
+      allowedHosts: [""] 
     },
     plugins: [
       VitePWA({
         registerType: 'autoUpdate',
-        // 2. Force the service worker to generate during 'npm run dev'
         devOptions: {
           enabled: true,
           type: 'module'
@@ -25,12 +26,12 @@ export default defineConfig({
           theme_color: '#ffffff',
           icons: [
             {
-              src: '/pwa-192x192.png',
+              src: 'pwa-192x192.png',
               sizes: '192x192',
               type: 'image/png'
             },
             {
-              src: '/pwa-512x512.png',
+              src: 'pwa-512x512.png',
               sizes: '512x512',
               type: 'image/png'
             }
